4f5a0e17f636603d429e05c7745b04462bb8d171
BrunoOller <brunobrunelli4@gmail.com>
2026-04-06 10:16:34 -0300

    Adicionando a aula do dia 06/04

---
+++ TP2/Delegate/Delegate/Delegate/Program.cs
@@ -0,0 +1,18 @@
+﻿// Segunda Etapa
+MeuDelegate del1 = new MeuDelegate(MeuMetodo);
+MeuDelegate del2 = MeuMetodo;
+MeuDelegate del3 = (msg) => Console.WriteLine(msg);
+
+// Terceira Etapa
+del1.Invoke("Minha mensagem 1");
+del2("Minha mensagem 2");
+del3("Minha mensagem 3");
+
+static void MeuMetodo(string mensagem)
+{
+    Console.WriteLine(mensagem);
+}
+Console.ReadKey();
+
+// Primeira Etapa
+public delegate void MeuDelegate(string msg);
---
+++ TP2/Delegate/Delegate/Delegate_Eventos/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Usando Evento");
+Pedido pedido = new Pedido();
+pedido.OnCriarPedido += EnviarEmail.email;
+pedido.OnCriarPedido += EnviarSMS.sms;
+
+pedido.criarPedido();
+
+Console.WriteLine("Fim do Pedido");
+
+Console.ReadLine();
+delegate void PedidoEvent();
+class Pedido
+{
+    public event PedidoEvent? OnCriarPedido;
+    public void criarPedido()
+    {
+        Console.WriteLine("Iniciando o criar pedido");
+        if (OnCriarPedido != null)
+        {
+            OnCriarPedido();
+        }
+    }
+}
+class EnviarEmail
+{
+    public static void email()
+    {
+        Console.WriteLine("Email enviado!");
+    }
+}
+
+class EnviarSMS
+{
+    public static void sms()
+    {
+        Console.WriteLine("SMS enviado!");
+    }
+}
