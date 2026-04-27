607ba76d9004be5da337f87f59712b3b263f01c1
BrunoOller <brunobrunelli4@gmail.com>
2026-04-27 09:53:36 -0300

    Adicionando o word com exercícios para p1

---
+++ TP2/ManipulacaoDeArquivos/ManipulacaoDeArquivos/Program.cs
@@ -0,0 +1,10 @@
+﻿var caminho = @"C:\Users\0201392511015\Desktop\TecnicasDeProgramacao\TP2\ManipulacaoDeArquivos\ManipulacaoDeArquivos\arquivo1.txt";
+
+if (!File.Exists(caminho)) {
+    File.WriteAllText(caminho, "Autor Desconhecido");
+}
+
+var novoTexto = "\nQuem conta seus vales espanta" + Environment.NewLine + "Água mole em pedra dura tanto bate até que fura \n Casa de ferreiro espeto é";
+File.AppendAllText(caminho, novoTexto);
+string conteudo = File.ReadAllText(caminho);
+Console.WriteLine(conteudo);
